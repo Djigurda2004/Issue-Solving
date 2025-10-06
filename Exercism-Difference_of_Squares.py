af3a29d6e32c2d70c4d7a5f91c73abfa9e333836
Djigurda2004 <primocar2030@gmail.com>
2025-10-06 19:08:58 +0200

    new commit

--- Exercism-Difference_of_Squares.py
+++ Exercism-Difference_of_Squares.py
@@ -1,16 +1,10 @@
 def square_of_sum(number):
-    sum = 0
-    for digit in range(number+1):
-        sum += digit
-    return sum**2
+    return sum([digit for digit in range(number+1)])**2
 
 
 def sum_of_squares(number):
-    sum = 0
-    for digit in range(number+1):
-        sum += (digit**2)
-    return sum
+    return sum([digit**2 for digit in range(number+1)])
 
 
 def difference_of_squares(number):
-    return  square_of_sum(number) - sum_of_squares(number)
+    return square_of_sum(number) - sum_of_squares(number)
